5d50ef27e6b3a6c35218868a6732cf06dace092f
kishow01 <kishow01@gmail.com>
2022-11-01 21:36:30 +0800

    ganache dependency

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # EthFuzzer
 EthFuzzer is a fuzzing tool for Ethereum's smart contract. unlike other smart contract fuzzing tools that use the EOA Account as the starting point to perform fuzzing test on the target contract, EthFuzzer uses the Contract Account as the starting point to perform fuzzing test on the target contract. This means that EthFuzzer generates a variety of contracts, interacts the generated contracts with the target contract, and observes whether there is a breach of security between the transactions.
 
+## prerequisite
+1. ``ganache-cli``
--- src/blockchain.py
+++ src/blockchain.py
@@ -5,9 +5,13 @@ from util import DEFAULT_BLOCKCHAIN_KEY_LOCATION, DEFAULT_BLOCKCHAIN_PORT
 class Ganache:
     def start(self):
         from time import sleep
-        self.proc = subprocess.Popen(['ganache-cli', '--account_keys_path', DEFAULT_BLOCKCHAIN_KEY_LOCATION, '--port', str(DEFAULT_BLOCKCHAIN_PORT)], 
-                                     stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-        sleep(5)
+
+        try:
+            self.proc = subprocess.Popen(['ganache-cli', '--account_keys_path', DEFAULT_BLOCKCHAIN_KEY_LOCATION, '--port', str(DEFAULT_BLOCKCHAIN_PORT)], 
+                                           stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+            sleep(3)
+        except FileNotFoundError:
+            print('[*] Missing dependency: ganahce-cli')
 
     def end(self):
         self.proc.terminate()
