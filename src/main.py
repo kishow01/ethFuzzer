5c3a3977550e874579372342199ce69d2f5a465a
kishow01 <kishow01@gmail.com>
2022-11-02 22:06:05 +0800

    oracle ignores reverted transaction

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from ethfuzzer import EthFuzzer
 
-"""from test import testc_contract_name, testc_solidity_version, testc_source_code"""
+# from test import testc_contract_name, testc_solidity_version, testc_source_code
 from test import erc_contract_name, erc_solidity_version, erc_source_code
 
 ethFuzzer = EthFuzzer()
 
 try:
     # create and deploy test contract
-    """ethFuzzer.create_testc(testc_source_code, testc_contract_name, testc_solidity_version)"""
+    # ethFuzzer.create_testc(testc_source_code, testc_contract_name, testc_solidity_version)
     ethFuzzer.create_testc(erc_source_code, erc_contract_name, erc_solidity_version)
     print('[*] test contract deployed at:', ethFuzzer.testc_address)
         
@@ -26,7 +26,8 @@ try:
                 break
 
             # Detects if there is a violation during execution
-            ethFuzzer.oracle_detect(testc_trace, atkc_source_code, tx_hash)
+            if coverage != set():
+                ethFuzzer.oracle_detect(testc_trace, atkc_source_code, tx_hash)
 
             print('.', end='', flush=True)
         print('')
